f6ead99b7bee8ef18e94b652480d3e6ebc3419e3
tjaiswal <tjaiswal@tjaiswal-VAIO>
2017-09-04 20:40:15 +0530

    add

--- src/test/java/com/guru/commons/testscripts/TC005_PurchasePurducts.java
+++ src/test/java/com/guru/commons/testscripts/TC005_PurchasePurducts.java
@@ -1,4 +1,28 @@
+/*  Verify user is able to purchase product using registered email id(USE CHROME BROWSER)     
+Test Steps:
+1. Go to http://live.guru99.com/
+2. Click on my account link
+3. Login in application using previously created credential
+4. Click on MY WISHLIST link 
+5. In next page, Click ADD TO CART link
+6. Enter general shipping country, state/province and zip for the shipping cost estimate
+7. Click Estimate 
+8. Verify Shipping cost generated 
+9. Select Shipping Cost, Update Total 
+10. Verify shipping cost is added to total 
+11. Click "Proceed to Checkout"
+12a. Enter Billing Information, and click Continue
+12b. Enter Shipping Information, and click Continue
+13. In Shipping Method, Click Continue
+14. In Payment Information select 'Check/Money Order' radio button. Click Continue
+15. Click 'PLACE ORDER' button 
+16. Verify Oder is generated. Note the order number
+
+NOTE: PROCEED TO CHECKOUT (step 6 ) was taken out, so as to allow the Estimate button step to get processed. 
+      Rest of the steps renumbered accordingly. 
+*/
 package com.guru.commons.testscripts;
+
 
 import java.util.Properties;
 
